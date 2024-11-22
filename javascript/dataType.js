f1c0fb8939f5ec7b6c07848fc4aa8b1c98c73329
Aashu01a <120313158+Aashu01a@users.noreply.github.com>
2024-11-22 21:57:46 +0530

    Add files via upload

---
+++ javascript/dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js coade as new version
+
+// alert("hello") we are usaingf node js, not browser rn '
+console.log(2+6);
+                        //code readiblity needs to be high
+console.log("jeanne");
+
+
+let name = "jeanne"
+let age = 10
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone valude
+//undefined => value is not assigned yet
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
