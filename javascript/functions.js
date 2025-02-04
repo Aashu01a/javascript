e07c4c68a314d00af9527abe38e12a0ebd0753bf
Aashu01a <120313158+Aashu01a@users.noreply.github.com>
2025-02-04 14:09:55 +0530

    Add files via upload

---
+++ javascript/functions.js
@@ -0,0 +1,59 @@
+//function greet () {//
+    //console.log("hello world");
+    //}
+    //
+   /// let displayPI = function () {
+    //    console.log("PI =3.14");
+    //}
+    //greet () ;
+
+    //displayPI();
+
+//function area(x,y) {
+ ////   return(x*y)
+//}
+//console.log(area(3,2));
+
+//let i= prompt("marks for i")
+  // //let j= prompt("marks for j")
+   // k =prompt("marks for k")
+   //let z= prompt("marks for z")
+   //let l = prompt("marks for l")
+
+//function marks(i,j,k,z,l) {
+   
+    //return(((i+j+k+z+l)/500)*100)
+//}
+//alert(marks(i,j,k,z,l))
+//console.log(marks(i,j,k,z,l))
+
+if(true) 
+{
+    let x = 10;
+    const y = 20;
+    console.log(x, y); // output will be 10 20
+}
+
+const name = "Jean";
+const age = 21;
+console.log(`Hello, my name is ${name} and my age is ${age}`); 
+
+//const add = (a,b) => a+b;
+//console.log(add(5,3));
+ 
+//const person = {name :"jean", age:22};
+//const{name,age1}= person;
+//console.log(name,age); //it wont work bc i had alr defined the name at 37
+
+//const numbers = [1,2,3];
+//const [a,b,c] = numbers;
+//console.log(a,b,c); //output :1 2 3
+
+
+class person {
+    greeet () {
+        console.log(`hello, my name is ${name}`);
+    }
+}
+const artoria = new person("artoria",20);
+artoria.greeet(); // 
